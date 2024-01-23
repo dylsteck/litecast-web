360e6e42435d4ed6113a81ea8ef378512c7d940c
dylsteck <dylancsteck@gmail.com>
2024-01-23 13:37:16 -0500

    [fix] restrict ModEditor access to authenticated users

--- app/components/CastFeed.tsx
+++ app/components/CastFeed.tsx
@@ -1,15 +1,16 @@
 "use client"
-import { useLatestCasts } from "../providers/NeynarProvider"
+import { useLatestCasts, useLogin } from "../providers/NeynarProvider"
 import { NeynarCastV2 } from "../types";
 import CastFeedItem from "./CastFeedItem";
 import ModEditor from './ModEditor';
 
 export default function CastFeed(){
     const { casts, isLoading, isReachingEnd, loadMore } = useLatestCasts();
+    const  { farcasterUser } = useLogin();
     // note: should add error to this 
     return(
         <div className="flex flex-col w-full gap-1 pb-4 overflow-y-auto">
-            <ModEditor />
+            {farcasterUser && <ModEditor />}
             {casts && casts.map((item: NeynarCastV2, index) => {
                 return <CastFeedItem cast={item} key={`cast-feed-item-${index}`} />
             })}
