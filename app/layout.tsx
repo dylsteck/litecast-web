63d81544fbb8fabbe779cf89521629cbadd22f43
dylsteck <dylancsteck@gmail.com>
2024-01-23 00:52:46 -0500

    Adds Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }){
   return (
     <html lang="en">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <body className={GeistSans.className}>
         <Providers>
           {children}
