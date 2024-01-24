47c9c5c3ed6542bdf39af0341be76ca9588e61c1
David Furlong <dvfurlong@googlemail.com>
2024-01-24 16:17:26 +0100

    fix: mod editor styling bugs, embedseditor, non unique keys, castlengthui

--- app/components/CastFeed.tsx
+++ app/components/CastFeed.tsx
@@ -1,8 +1,8 @@
-import { FunctionComponent } from 'react';
-import { useLatestCasts, useLogin } from "../providers/NeynarProvider"
+import { FunctionComponent } from "react";
+import { useLatestCasts, useLogin } from "../providers/NeynarProvider";
 import { NeynarCastV2 } from "../types";
 import CastFeedItem from "./CastFeedItem";
-import ModEditor from './ModEditor';
+import ModEditor from "./ModEditor";
 
 interface CastFeedProps {
   castEditorVisible?: boolean;
@@ -13,17 +13,20 @@ const CastFeed: FunctionComponent<CastFeedProps> = ({
   castEditorVisible = false,
   username,
 }) => {
-    const { casts, isLoading, isReachingEnd, loadMore } = useLatestCasts();
-    const  { farcasterUser } = useLogin();
-    // note: should add error to this 
-    return(
-        <div className="flex flex-col w-full gap-1 pb-4 overflow-y-auto">
-            {farcasterUser && castEditorVisible && <ModEditor />}
-            {casts && casts.map((item: NeynarCastV2, index) => {
-                return <CastFeedItem cast={item} key={`cast-feed-item-${index}`} />
-            })}
-        </div>
-    )
-}
+  const { casts, isLoading, isReachingEnd, loadMore } = useLatestCasts();
+  const { farcasterUser } = useLogin();
+  // note: should add error to this
+  return (
+    <div className="flex flex-col w-full gap-1 pb-4 overflow-y-auto">
+      {farcasterUser && castEditorVisible && <ModEditor />}
+      {casts &&
+        casts.map((item: NeynarCastV2, index) => {
+          return (
+            <CastFeedItem cast={item} key={`cast-feed-item-${item.hash}`} />
+          );
+        })}
+    </div>
+  );
+};
 
 export default CastFeed;
--- app/components/ModEditor.tsx
+++ app/components/ModEditor.tsx
@@ -1,8 +1,20 @@
 import { useEditor, EditorContent } from "@mod-protocol/react-editor";
 import { EmbedsEditor } from "@mod-protocol/react-ui-shadcn/dist/lib/embeds";
 import { Button } from "@mod-protocol/react-ui-shadcn/dist/components/ui/button";
-import { Embed, ModManifest, fetchUrlMetadata, handleAddEmbed, handleOpenFile, handleSetInput } from "@mod-protocol/core";
-import { Channel, getFarcasterChannels, getFarcasterMentions } from "@mod-protocol/farcaster";
+import {
+  Embed,
+  ModManifest,
+  fetchUrlMetadata,
+  handleAddEmbed,
+  handleOpenFile,
+  handleSetInput,
+} from "@mod-protocol/core";
+import {
+  Channel,
+  getFarcasterChannels,
+  getFarcasterMentions,
+  isFarcasterUrlEmbed,
+} from "@mod-protocol/farcaster";
 import { MentionList } from "@mod-protocol/react-ui-shadcn/dist/components/mention-list";
 import { ChannelList } from "@mod-protocol/react-ui-shadcn/dist/components/channel-list";
 import { ChannelPicker } from "@mod-protocol/react-ui-shadcn/dist/components/channel-picker";
@@ -10,51 +22,74 @@ import { CastLengthUIIndicator } from "@mod-protocol/react-ui-shadcn/dist/compon
 import { createRenderMentionsSuggestionConfig } from "@mod-protocol/react-ui-shadcn/dist/lib/mentions";
 import { useLogin } from "../providers/NeynarProvider";
 import { defaultRichEmbedMod } from "@mod-protocol/mod-registry";
-import { renderers } from '@mod-protocol/react-ui-shadcn/dist/renderers';
+import { renderers } from "@mod-protocol/react-ui-shadcn/dist/renderers";
 import { RichEmbed } from "@mod-protocol/react";
 
 const MOD_API_URL = "https://api.modprotocol.org/api";
 const FCKIT_API_URL = "https://api.farcasterkit.com";
- 
+
 // use our MOD_API_URL, self host our API, or use your own API.
 const getUrlMetadata = fetchUrlMetadata(MOD_API_URL);
 const getResults = getFarcasterMentions(MOD_API_URL);
 const getChannels = getFarcasterChannels(MOD_API_URL);
- 
+
 // todo: make default state for hash null and change type to string | null
 export default function ModEditor({ hash }: { hash?: string }) {
   const { farcasterUser } = useLogin();
-  const initialText = hash ? 'Reply' : 'Cast something';
-  const onSubmit = async ({ text, embeds, channel }: { text: string, embeds: Embed[], channel: Channel }): Promise<boolean> => {
-    
+  const onSubmit = async ({
+    text,
+    embeds,
+    channel,
+  }: {
+    text: string;
+    embeds: Embed[];
+    channel: Channel;
+  }): Promise<boolean> => {
     try {
-      if(text.length === 0 || text === initialText){
-        throw new Error('You submitted a blank cast. Please try again.')
-        alert('You submitted a blank cast. Please try again.')
+      if (text.length === 0) {
+        throw new Error("You submitted a blank cast. Please try again.");
+        alert("You submitted a blank cast. Please try again.");
       }
       const respBody = {
         parent: hash,
         signer_uuid: farcasterUser?.signer_uuid,
         text: text,
-        channel_id: channel ? channel : ''
+        embeds: embeds.length
+          ? embeds.map((embed) =>
+              isFarcasterUrlEmbed(embed)
+                ? { url: embed.url }
+                : {
+                    cast_id: { ...embed.castId },
+                  }
+            )
+          : undefined,
+        channel_id: channel?.id ? channel?.id : "",
       };
       await fetch(`${FCKIT_API_URL}/neynar/cast`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(respBody),
       });
     } catch (error) {
-      console.error('Failed to post cast', error);
+      console.error("Failed to post cast", error);
       alert(error);
       return false;
     }
-    alert('Casted successfully!')
+    alert("Casted successfully!");
     return true;
   };
 
- const { editor, getEmbeds, setEmbeds, handleSubmit, getText, setChannel, getChannel } = useEditor({
+  const {
+    editor,
+    getEmbeds,
+    setEmbeds,
+    handleSubmit,
+    getText,
+    setChannel,
+    getChannel,
+  } = useEditor({
     fetchUrlMetadata: getUrlMetadata,
     onError: (error) => console.error(error),
     onSubmit,
@@ -67,27 +102,19 @@ export default function ModEditor({ hash }: { hash?: string }) {
       getResults: getResults,
       RenderList: MentionList,
     }),
-    initialText: initialText,
-    // TODO: fix placeholdertext, isn't showing up for some reason
+    initialText: "",
+    placeholderText: hash ? "Reply" : "Cast something",
   });
   return (
     <div className="border-b border-black z-index-50">
       <form onSubmit={handleSubmit}>
         <EditorContent
-            editor={editor}
-            autoFocus
-            className="w-full h-auto max-h-full p-3 pl-4"
+          editor={editor}
+          autoFocus
+          className="w-full h-auto max-h-full p-3 pl-4"
         />
-        <div className="flex flex-row gap-4 items-center p-3 pl-4">
-          {!hash && 
-            <ChannelPicker
-              getChannels={getChannels}
-              onSelect={setChannel}
-              value={getChannel()}
-            />
-          }
-          {/* TODO: Get EmbedsEditor and CastLengthUIIndicator to show up, not working ATM */}
-          {/* <EmbedsEditor
+        <div className="px-4 py-3">
+          <EmbedsEditor
             embeds={getEmbeds()}
             setEmbeds={setEmbeds}
             RichEmbed={({ embed }) => (
@@ -100,12 +127,25 @@ export default function ModEditor({ hash }: { hash?: string }) {
               />
             )}
           />
-          <CastLengthUIIndicator getText={getText} /> */}
-          <Button type="submit" className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 text-white bg-[#855DCD]">
+        </div>
+        <div className="flex flex-row gap-4 items-center p-3 pl-4">
+          {!hash && (
+            <ChannelPicker
+              getChannels={getChannels}
+              onSelect={setChannel}
+              value={getChannel()}
+            />
+          )}
+          <CastLengthUIIndicator getText={getText} />
+          <div className="grow"></div>
+          <Button
+            type="submit"
+            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 text-white bg-[#855DCD]"
+          >
             Cast
           </Button>
         </div>
       </form>
     </div>
-  )
-};
+  );
+}
