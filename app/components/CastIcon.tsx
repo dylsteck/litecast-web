fb92fca06c0f075015309e082521566b84468d6f
dylsteck <dylancsteck@gmail.com>
2024-01-23 23:16:38 -0500

    [bug] Fix CastIcon image link
    accidentally uploaded the wrong resolution image to imgur

--- app/components/CastIcon.tsx
+++ app/components/CastIcon.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function CastIcon({ width, height, onClick }: { width: number, height: number, onClick?: () => void }){
-    return <Image src="https://i.imgur.com/XVaksRG.png" alt="Cast icon" className={`w-${width} h-${height}`} onClick={onClick} />;
+    return <Image src="https://i.imgur.com/o3p5x1i.png" alt="Cast icon" width={width} height={height} className={`w-${width} h-${height}`} onClick={onClick} />;
 }
