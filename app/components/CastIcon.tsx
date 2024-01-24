63b601dee90cf5d095e2788c5159b69d53956991
dylsteck <dylancsteck@gmail.com>
2024-01-23 23:13:09 -0500

    [bug] switch CastIcon from file to link so it loads because of vercel image optimization

--- app/components/CastIcon.tsx
+++ app/components/CastIcon.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import castIcon from '../../public/castIcon.png';
 
 export default function CastIcon({ width, height, onClick }: { width: number, height: number, onClick?: () => void }){
-    return <Image src={castIcon} alt="Cast icon" className={`w-${width} h-${height}`} onClick={onClick} />;
+    return <Image src="https://i.imgur.com/XVaksRG.png" alt="Cast icon" className={`w-${width} h-${height}`} onClick={onClick} />;
 }
